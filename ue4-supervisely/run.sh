26b74da781210dd2562d716dd90605091106ed07
Lachlan Kermode <lachiekermode@gmail.com>
2019-05-30 15:17:12 +0100

    make run loop over multiple folders

--- ue4-supervisely/run.sh
+++ ue4-supervisely/run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-INPATH="/datasets/synthetic/UE4/generated_synthetic1"
-OUTPATH="/datasets/synthetic/supervisely/generated_synthetic1"
-DATASET_NAME="UE4 Triple Chasers (top)"
-LABEL="triplecanister_top"
 
-python lib/convert_masks.py --in "$INPATH" --out "$OUTPATH" --name "$DATASET_NAME" --label "$LABEL"
+BASE_PATH=/datasets/synthetic-datasets/Triple-Chaser
+folders=( "2019.04.15-FA-002" "2019.04.20-FA-003" "2019.04.23-FA-004" "2019.04.24-FA-001" "2019.04.25-FA-003" "2019.05.02-FA-002" )
+
+
+for FOLD in "${folders[@]}"
+do
+	INPATH="$BASE_PATH/$FOLD"
+	OUTPATH="$INPATH/supervisely"
+	DATASET_NAME="UE4 Triple Chasers (top)"
+	LABEL="triplecanister_top"
+
+	# NOTE: this line is only necessary if working straight from AA's exports.
+	# python lib/reorder.py --folder "$INPATH"
+	python lib/convert_masks.py \
+		--infolder "$INPATH" \
+		--outfolder "$OUTPATH" \
+		--name "$DATASET_NAME" \
+		--label "$LABEL"
+done
