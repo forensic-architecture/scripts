d730f314ab4d225eca80d037e1130fb7c8145363
Lachlan Kermode <lachiekermode@gmail.com>
2019-10-22 11:49:57 +0100

    add prepare folder script

---
+++ ue4-supervisely/prepare_folder.sh
@@ -0,0 +1,9 @@
+mkdir $2
+mkdir $2/raw_ann
+mkdir $2/img
+
+cp $1/RTXCustomStencil* $2/raw_ann/
+cp $1/RTXFinalImage* $2/img/
+
+cd $2/raw_ann && rename 's/RTXCustomStencil\.//' *
+cd $2/img && rename 's/RTXFinalImage\.//' *
