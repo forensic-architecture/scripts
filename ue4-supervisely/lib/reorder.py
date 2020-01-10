ed1068afbe92174f5f4cc8449928eee763af74cd
Lachlan Kermode <lachiekermode@gmail.com>
2020-01-10 02:59:52 +0000

    add unity format

--- ue4-supervisely/lib/reorder.py
+++ ue4-supervisely/lib/reorder.py
@@ -1,9 +1,10 @@
 import argparse
-from os import listdir, mkdir
+import re
 import shutil
+from os import listdir, mkdir, rmdir
 from os.path import isfile, join, normpath, basename
 
-def reorder(fname):
+def reorder_unreal(fname):
     files = [f for f in listdir(fname) if isfile(join(fname, f))]
     folder_name = basename(normpath(fname))
     img_folder = join(fname, 'img')
@@ -19,11 +20,44 @@ def reorder(fname):
             shutil.move(join(fname, f), join(img_folder, f.replace("{}FinalImage.".format(folder_name), "")))
 
 
+def reorder_unity(fname):
+    bl = ['_READ-ME.txt', 'config.json']
+    imgs = [f for f in listdir(fname) if isfile(join(fname, f)) and f not in bl]
+    masks_dir = join(fname, '_masks')
+    masks = listdir(masks_dir)
+    
+    img_folder = join(fname, 'img')
+    raw_ann_folder = join(fname, 'raw_ann')
+    try:
+        mkdir(img_folder)
+        mkdir(raw_ann_folder)
+    except: 
+        pass
+    
+    for f in imgs:
+        shutil.move(join(fname, f), join(img_folder, f))
+        
+    for f in listdir(join(fname, '_masks')):
+        newname = re.search(r'(.+)\_mask', f).group(1)
+        shutil.move(join(masks_dir, f), f"{join(raw_ann_folder, newname)}.jpg")
+        
+    try:
+        rmdir(masks_dir)
+    except:
+        pass
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--folder", help="folder to reorder in prep for convert_masks.py")
+    parser.add_argument("--format", help="format to convert from: either 'unity' or 'unreal'")
 
     args = parser.parse_args()
 
-    reorder(args.folder)
+    if args.format == 'unity':
+        reorder_unity(args.folder)
+    elif args.format == 'unreal':
+        reorder_unreal(args.folder)
+
+
--- ue4-supervisely/run.sh
+++ ue4-supervisely/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BASE_PATH=/datasets/synthetic-datasets/Triple-Chaser
+BASE_PATH=/workspace/datasets/_sy
 
 for FOLD in 2019.04.24-FA-001 2019.04.25-FA-003 2019.05.02-FA-002
 do
@@ -8,9 +8,10 @@ do
 	OUTPATH="$INPATH/supervisely"
 	DATASET_NAME="UE4 Triple Chasers (top)"
 	LABEL="triplecanister_top"
+	FORMAT="unreal"
 
-	# NOTE: this line is only necessary if working straight from AA's exports.
-	# python lib/reorder.py --folder "$INPATH"
+	# NOTE: this line is only necessary if working straight from unreal or unity exports.
+	# python lib/reorder.py --folder "$INPATH" --format "$FORMAT"
 	python lib/convert_masks.py \
 		--infolder "$INPATH" \
 		--outfolder "$OUTPATH" \
